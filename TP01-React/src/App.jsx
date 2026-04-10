afb7b38616da3dd7d836863e0173d1b6e163b986
FELIPE BECKFORD SISIS <49255481@ort>
2026-04-10 11:42:13 -0300

    Proyecto Terminado

--- TP01-React/src/App.jsx
+++ TP01-React/src/App.jsx
@@ -1,20 +1,22 @@
-import { useState } from 'react'
 import './App.css'
 import Formulario from './components/Formulario'
 import Listado from './components/Listado'
 
 function App() {
   return (
-    <>
+    <div className="app-wrapper">
       <h1>Administrador de Pacientes</h1>
 
       <div className="app-container">
-        <Formulario sub="Crear mi cita" />
-        <Listado sub="Administra tus citas" />
+        <div className="col-left">
+          <Formulario sub="Crear mi cita" />
+        </div>
+        <div className="col-right">
+          <Listado sub="Administra tus citas" />
+        </div>
       </div>
-    </>
+    </div>
   )
 }
 
 export default App
-
--- TP01-React/src/components/Listado/index.jsx
+++ TP01-React/src/components/Listado/index.jsx
@@ -4,16 +4,16 @@ import Subtitulo from '../Subtitulo';
 
 const Listado = ({ sub }) => {
   const citas = [
-    { nMascota: "Floki", dueno: "Ari", fecha: '2023-08-05', hora: '16:15', sintomas: "No está comiendo" },
+    { nMascota: "Nina", dueno: "Martin", fecha: '2021-08-05', hora: '08:20', sintomas: "Le duele la pierna" },
     { nMascota: "Sifon", dueno: "Flecha", fecha: '2023-08-05', hora: '09:24', sintomas: "Duerme mucho" },
-    { nMascota: "Nina", dueno: "Martin", fecha: '2021-08-05', hora: '08:20', sintomas: "Le duele la pierna" }
+    { nMascota: "Floki", dueno: "Ari", fecha: '2023-08-05', hora: '16:15', sintomas: "No está comiendo" }
   ];
 
   return (
     <>
       <Subtitulo sub={sub} />
       <div className="listado-wrapper">
-        {citas.map(c => <Cita objCita={c} />)}
+        {citas.map((c, i) => <Cita key={i} objCita={c} />)}
       </div>
     </>
   );
