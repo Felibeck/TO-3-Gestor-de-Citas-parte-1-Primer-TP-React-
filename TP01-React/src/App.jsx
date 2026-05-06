d521044556d1c90c042d284fc544e325a4adc57a
FELIPE BECKFORD SISIS <49255481@ort>
2026-05-06 09:57:35 -0300

    useEffect aplicado con localstorage

--- TP01-React/src/App.jsx
+++ TP01-React/src/App.jsx
@@ -1,16 +1,24 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Formulario from './components/Formulario'
 import Listado from './components/Listado'
 
 function App() {
-
-  const [citas, setCitas] = useState([
+  
+  localStorage.setItem('citas', JSON.stringify([
     { id: 1, mascota: "Nina", propietario: "Martin", fecha: '2021-08-05', hora: '08:20', sintomas: "Le duele la pierna" },
     { id: 2, mascota: "Sifon", propietario: "Flecha", fecha: '2023-08-05', hora: '09:24', sintomas: "Duerme mucho" },
     { id: 3, mascota: "Floki", propietario: "Ari", fecha: '2023-08-05', hora: '16:15', sintomas: "No está comiendo" }
-  ])
+  ]))
   
+  const [citas, setCitas] = useState(JSON.parse(localStorage.getItem('citas')))
+  
+  useEffect(() => 
+  {
+    localStorage.setItem('citas', JSON.stringify(citas))
+  }, [citas])
+
+
   return (
     <div className="app-wrapper">
       <h1>Administrador de Pacientes</h1>
